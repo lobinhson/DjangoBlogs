7cf851276659121c0528867e287b7f4eee9d3d4a
lobinhson <lobinhson@gmail.com>
2025-04-14 14:22:37 +0700

    Update settings.py

--- personal_blog/settings.py
+++ personal_blog/settings.py
@@ -23,9 +23,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-+k8c$n*sdoakur#4#5#_wdl_9m%5fdj($k#(7i-rwg5s2^kgni'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+# ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'lobinhson.pythonanywhere.com']
 
 
 # Application definition
